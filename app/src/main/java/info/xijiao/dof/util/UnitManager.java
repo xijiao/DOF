4bd0433c25d10554f2eada0aaefee80846ca2068
xijiao <xijiaosdq@gmail.com>
2016-03-07 14:58:36 +0800

    Optimize distance display

--- app/src/main/java/info/xijiao/dof/util/UnitManager.java
+++ app/src/main/java/info/xijiao/dof/util/UnitManager.java
@@ -10,6 +10,8 @@ package info.xijiao.dof.util;
 
 import android.content.Context;
 
+import java.text.DecimalFormat;
+
 import info.xijiao.dof.R;
 import info.xijiao.dof.Constants;
 
@@ -35,54 +37,30 @@ public class UnitManager implements Constants {
         return context.getString(DISTANCE_UNIT_NAME[distanceUnit.ordinal()]);
     }
 
-    public String getDistanceText(Context context, float distance) {
-        if (distance == Float.POSITIVE_INFINITY) {
+    public String getCompatDistanceText(Context context, float v) {
+        if (v == Float.POSITIVE_INFINITY) {
             return context.getString(R.string.infinity);
         }
-        else if (distance == Float.NEGATIVE_INFINITY) {
+        else if (v == Float.NEGATIVE_INFINITY) {
             return context.getString(R.string.negative_infinity);
         }
-        else if (distance == Float.NaN) {
+        else if (v == Float.NaN) {
             return context.getString(R.string.no_a_number);
         }
 
-        if (mUnitType == UNIT_SYSTEM.METRIC) {
-            if (distance >= getDistanceUnitLength(DISTANCE_UNIT.METERS)) {
-                return String.format("%.2f%s",
-                        distance / getDistanceUnitLength(DISTANCE_UNIT.METERS),
-                        getDistanceUnitName(context, DISTANCE_UNIT.METERS));
-            }
-            else {
-                return String.format("%.3g%s",
-                        distance / getDistanceUnitLength(DISTANCE_UNIT.CENTIMETERS),
-                        getDistanceUnitName(context, DISTANCE_UNIT.CENTIMETERS));
-
-            }
-        }
-        else {
-            if (distance >= getDistanceUnitLength(DISTANCE_UNIT.FEET)) {
-                return String.format("%.2f%s",
-                        distance / getDistanceUnitLength(DISTANCE_UNIT.FEET),
-                        getDistanceUnitName(context, DISTANCE_UNIT.FEET));
-            }
-            else {
-                return String.format("%.3g%s",
-                        distance / getDistanceUnitLength(DISTANCE_UNIT.INCHES),
-                        getDistanceUnitName(context, DISTANCE_UNIT.INCHES));
-
-            }
-        }
-    }
-
-    public String getCompatDistanceText(Context context, float v) {
+        DecimalFormat formater = new DecimalFormat("#.#");
         if (v < 0.01f) {
-            return String.format("%.0f%s", v * 1000.0f, context.getResources().getString(R.string.millimeter));
+            return String.format("%s%s", formater.format(v * 1000.0f),
+                    context.getResources().getString(R.string.millimeter));
         } else if (v < 1.0f) {
-            return String.format("%.0f%s", v * 100.0f, context.getResources().getString(R.string.centimeter));
+            return String.format("%s%s", formater.format(v * 100.0f),
+                    context.getResources().getString(R.string.centimeter));
         } else if (v < 1000f) {
-            return String.format("%.0f%s", v, context.getResources().getString(R.string.meter));
+            return String.format("%s%s", formater.format(v),
+                    context.getResources().getString(R.string.meter));
         } else {
-            return String.format("%.0f%s", v / 1000.0f, context.getResources().getString(R.string.kilometer));
+            return String.format("%s%s", formater.format(v / 1000.0f),
+                    context.getResources().getString(R.string.kilometer));
         }
     }
 }
